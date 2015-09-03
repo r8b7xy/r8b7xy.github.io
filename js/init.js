1410fa0beb04315c40e2b3f2a9b291ff8e60098e
Rafal Blaszczyk <rafbl@autistici.org>
2015-09-03 17:59:32 +0200

    update

--- js/init.js
+++ js/init.js
@@ -1,6 +1,11 @@
-$('.button-collapse').sideNav({
-  menuWidth: 200, // Default is 240
-  edge: 'left', // Choose the horizontal origin
-  closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-}
-);
+(function($){
+  $(function(){
+
+  $('.button-collapse').sideNav({
+      menuWidth: 200, // Default is 240
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+
+  );
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
