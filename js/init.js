fe26e0433768c499ff85c1148d465aa0588f337b
Rafal Blaszczyk <rafbl@autistici.org>
2015-09-03 16:44:50 +0200

    update

--- js/init.js
+++ js/init.js
@@ -1,4 +1,6 @@
- // Initialize collapse button
-  $(".button-collapse").sideNav();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  $('.collapsible').collapsible();
+  $('.button-collapse').sideNav({
+      menuWidth: 200, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
