238213be197525c5ed479fdf6b0d4060109c4c65
Rafal Blaszczyk <rafbl@autistici.org>
2015-09-05 01:09:47 +0200

    update

--- js/init.js
+++ js/init.js
@@ -1,7 +1,18 @@
 (function($){
-  $(function(){
+    $(function(){
 
-    $('.button-collapse').sideNav();
+        $('header').bind('click', 'ul li a', function(event) {
+            $.scrollTo(event.target.hash, 250);
+        });
 
-  }); // end of document ready
+        // Plugin initialization
+        $('.slider').slider({full_width: true});
+        $('.parallax').parallax();
+        $('.modal-trigger').leanModal();
+        $('.scrollspy').scrollSpy();
+        $('.button-collapse').sideNav({closeOnClick: true});
+        $('.datepicker').pickadate({selectYears: 20});
+        $('select').not('.disabled').material_select();
+
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
