dc8027d5a0568bd2b17f02e8ec46c53d755211aa
Rafal Blaszczyk <rafbl@autistici.org>
2015-09-04 21:47:14 +0200

    update

--- js/init.js
+++ js/init.js
@@ -1,9 +1,20 @@
-$('.button-collapse').sideNav({
-    menuWidth: 200, // Default is 240
-    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-});
-// $(function() {
-    $('header').bind('click', 'ul li a', function(event) {
-        $.scrollTo(event.target.hash, 250);
-    });
-// });
+(function($){
+    $(function(){
+
+        /*
+         * $('header').bind('click', 'ul li a', function(event) {
+         *     $.scrollTo(event.target.hash, 250);
+         * });
+         */
+
+        // Plugin initialization
+        $('.slider').slider({full_width: true});
+        $('.parallax').parallax();
+        $('.modal-trigger').leanModal();
+        $('.scrollspy').scrollSpy();
+        $('.button-collapse').sideNav({closeOnClick: true});
+        $('.datepicker').pickadate({selectYears: 20});
+        $('select').not('.disabled').material_select();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
