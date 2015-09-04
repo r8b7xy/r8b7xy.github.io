eaeaa0929eaf1a50b8935c9479f6535e220a20a2
Rafal Blaszczyk <rafbl@autistici.org>
2015-09-04 16:45:52 +0200

    update

--- js/init.js
+++ js/init.js
@@ -2,3 +2,8 @@ $('.button-collapse').sideNav({
     menuWidth: 200, // Default is 240
     closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
 });
+// $(function() {
+    $('header').bind('click', 'ul li a', function(event) {
+        $.scrollTo(event.target.hash, 250);
+    });
+// });
