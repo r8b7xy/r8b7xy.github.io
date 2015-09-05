5614f6debcc94dc6c7d60202afb68890e021c0ff
Rafal Blaszczyk <rafbl@autistici.org>
2015-09-05 12:37:50 +0200

    udate

--- js/init.js
+++ js/init.js
@@ -1,7 +1,10 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+  $('.button-collapse').sideNav({
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
