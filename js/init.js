ffabd4cfa7d63c66ed0428c7b6a4938ce2e9aa07
Rafal Blaszczyk <rafbl@autistici.org>
2015-09-05 23:58:25 +0200

    udate

--- js/init.js
+++ js/init.js
@@ -9,9 +9,8 @@
   }); // end of document ready
 
   var options = [
-    {selector: '#nav_info', offset: 300, callback: 'Materialize.fadeInImage("#nav_info")' },
     {selector: '#nav_contact', offset: 500, callback: 'Materialize.fadeInImage("#nav_contact")' },
-    {selector: '#nav_map', offset: 300, callback: 'Materialize.fadeInImage("#nav_map")' },
+    {selector: '#nav_map', offset: 500, callback: 'Materialize.fadeInImage("#nav_map")' },
   ];
   Materialize.scrollFire(options);
 
